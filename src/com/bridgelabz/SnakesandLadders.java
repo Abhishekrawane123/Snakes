deeac8ef457cf3503d96afa25bd867d3dddb18ba
pknowledge <abhishekrawane1195@gmail.com>
2021-11-01 22:21:40 +0530

    Updated: program with uc2

--- src/com/bridgelabz/SnakesandLadders.java
+++ src/com/bridgelabz/SnakesandLadders.java
@@ -6,6 +6,9 @@ public class SnakesandLadders
 	{
 		int position = 0;
 		System.out.println("The Position of the Player is: "+position);
+		int dieCheck = ((int)(Math.random()*10)%6)+1;
+		System.out.println("The number on die is: "+dieCheck);
+
 	}
 
 }
