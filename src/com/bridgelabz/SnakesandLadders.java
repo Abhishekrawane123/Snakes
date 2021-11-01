804589e803c1b2f5a458598995ffd26cd0d06381
pknowledge <abhishekrawane1195@gmail.com>
2021-11-01 22:13:00 +0530

    added: first program

---
+++ src/com/bridgelabz/SnakesandLadders.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class SnakesandLadders 
+{
+	public static void main(String[] args) 
+	{
+		int position = 0;
+		System.out.println("The Position of the Player is: "+position);
+	}
+
+}
