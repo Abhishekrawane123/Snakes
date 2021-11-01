4fe185d381d36262f02b0bc219a4e97a684122b1
pknowledge <abhishekrawane1195@gmail.com>
2021-11-01 22:06:23 +0530

    added: .gitignore welcome files

---
+++ src/com/bridgelabz/WelcomeProgram.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class WelcomeProgram {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to Snakes & Ladders Program");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module Snakes {
+}
