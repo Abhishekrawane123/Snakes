f914e200c9b45e008f3c23c703712779f36aec4e
pknowledge <abhishekrawane1195@gmail.com>
2021-11-01 22:28:37 +0530

    updated: merging with uc3

--- src/com/bridgelabz/SnakesandLadders.java
+++ src/com/bridgelabz/SnakesandLadders.java
@@ -2,13 +2,36 @@ package com.bridgelabz;
 
 public class SnakesandLadders 
 {
-	public static void main(String[] args) 
+	public static final int NO_PLAY = 0;
+	public static final int LADDER = 1;
+	public static final int SNAKE = 2;
+	
+	public static void main(String[] args)
 	{
 		int position = 0;
-		System.out.println("The Position of the Player is: "+position);
-		int dieCheck = ((int)(Math.random()*10)%6)+1;
-		System.out.println("The number on die is: "+dieCheck);
-
+		int dieCheck;
+		int optionCheck;
+		while(position<100)
+		{
+			dieCheck = ((int) ((Math.random())*10)%6)+1;
+			optionCheck =(int) ((Math.random())*10)%3;
+			switch(optionCheck)
+			{
+			case NO_PLAY:
+				position=position;
+				break;
+			case LADDER:
+				position=position+dieCheck;
+				break;
+			default:
+				position=position-dieCheck;
+				if(position<=0)
+				{
+					position=0;
+				}
+			}
+		}
+		System.out.println("The Position is: "+position);
 	}
 
 }
